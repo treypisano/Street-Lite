902591aef1a7636caebc836479c039f90fa97e85
harjitsingh3 <singh.harjit09@gmail.com>
2023-06-12 11:51:06 -0400

    Create and add logo

--- frontend/src/components/Navigation/NavigationBar.js
+++ frontend/src/components/Navigation/NavigationBar.js
@@ -1,8 +1,13 @@
 import React from "react";
+import logogreen from "../../images/street_lite_logo_green.png";
+import './navigation.css';
 
 function NavigationBar () {
     return (
         <div>
+            <div className="logo">
+                <img src={logogreen} className="logo"></img>              
+            </div>
             <p>Navigation Bar</p>
         </div>
     )
