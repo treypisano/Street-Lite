f8983ac187a72a3bbce99498960af8b8cd00aa76
harjitsingh3 <singh.harjit09@gmail.com>
2023-06-12 15:35:14 -0400

    Create Image Carousel

--- frontend/src/components/Homepage/Homepage.js
+++ frontend/src/components/Homepage/Homepage.js
@@ -1,14 +1,58 @@
 import React from "react";
+import Slider from "react-slick";
 import NavigationBar from "../Navigation/NavigationBar";
+import logogreen from "../../images/street_lite_logo_green.png";
+import logoyellow from "../../images/street_lite_logo_yellow.png";
 import './homepage.css';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 
 function Homepage() {
+    
+    const BackArrow = (props) => {
+        return (
+            <div className="back-arrow" onClick={props.onClick}>
+                Previous
+            </div>
+        )
+      }
+
+      const NextArrow = (props) => {
+        return (
+            <div className="next-arrow" onClick={props.onClick}>
+                Next
+            </div>
+        )
+      }
+    
+    let sliderSettings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        prevArrow: true,
+        nextArrow: true,
+        prevArrow: <BackArrow />,
+        nextArrow: <NextArrow />
+      };
+    
     return (
         <div className="homepage">
             <NavigationBar />
             <h1>Welcome to Street_Lite</h1>
-            <div>
-                Empty Divs
+            <div className="carousel">
+                <Slider {...sliderSettings}>
+                    <div>
+                        <img src={logogreen} className="carousel-image" alt="Green Logo" />
+                    </div>
+                    <div>
+                        <img src={logoyellow} className="carousel-image" alt="Yellow Logo" />
+                    </div>
+                </Slider>
             </div>
         </div>
     )
