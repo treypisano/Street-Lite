3bb52a8d89f6159decc6b21269d106057a1544e3
treypisano <treypisano@treypc>
2023-06-12 10:18:09 -0400

    add backend

---
+++ backend/routes/api/users.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
+const passport = require('passport');
+const { loginUser } = require('../../config/passport');
+
+router.get('/', function(req, res, next) {
+  res.json({
+    message: "GET /api/users"
+  });
+});
+
+router.post('/register', async (req, res, next) => {
+
+  const user = await User.findOne({
+    $or: [{ email: req.body.email }, { username: req.body.username }]
+  });
+
+  if (user) {
+    const err = new Error("Validation Error");
+    err.statusCode = 400;
+    const errors = {};
+    if (user.email === req.body.email) {
+      errors.email = "A user has already registered with this email";
+    }
+    if (user.username === req.body.username) {
+      errors.username = "A user has already registered with this username";
+    }
+    err.errors = errors;
+    return next(err);
+  }
+
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email
+  });
+
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) throw err;
+    bcrypt.hash(req.body.password, salt, async (err, hashedPassword) => {
+      if (err) throw err;
+      try {
+        newUser.hashedPassword = hashedPassword;
+        const user = await newUser.save();
+        return res.json(await loginUser(user));
+      }
+      catch(err) {
+        next(err);
+      }
+    })
+  });
+});
+
+// POST /api/users/login
+router.post('/login', async (req, res, next) => {
+  passport.authenticate('local', async function(err, user) {
+    if (err) return next(err);
+    if (!user) {
+      const err = new Error('Invalid credentials');
+      err.statusCode = 400;
+      err.errors = { email: "Invalid credentials" };
+      return next(err);
+    }
+    return res.json(await loginUser(user)); 
+  })(req, res, next);
+});
+module.exports = router;
