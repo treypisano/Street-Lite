f19315b9b84a52e4630a4e4604bda318d9ff5494
Arvid Hossain <arvidh97@gmail.com>
2023-06-12 17:05:46 -0400

    add like, event, comment models

---
+++ backend/models/Event.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const eventSchema = new Schema(
+  {
+    dates: [
+      {
+        type: Date,
+        required: true,
+      },
+    ],
+    startTime: {
+      type: Date,
+      required: true,
+    },
+    endTime: {
+      type: Date,
+      required: true,
+    },
+    location: {
+      startStreet: {
+        type: String,
+        required: true,
+      },
+      endStreet: {
+        type: String,
+        required: true,
+      },
+      mainStreet: {
+        type: String,
+        required: true,
+      },
+      latitude: {
+        type: String,
+        required: true,
+      },
+      longitude: {
+        type: String,
+        required: true,
+      },
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Event", eventSchema);
