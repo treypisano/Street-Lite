b26e6e36fc9f7fdc27f0934e3963403fc331f974
harjitsingh3 <singh.harjit09@gmail.com>
2023-06-13 17:33:24 -0400

    Add navigation links to footer

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -1,12 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom/cjs/react-router-dom";
 import './footer.css';
 
 function Footer() {
     return (
         <div className="footer">
-            <footer>
-                Footer
-            </footer>
+            <Link to={'/'}>
+                Homepage
+            </Link>
+            <br></br>
+            <Link to={'/events'}>
+                Events
+            </Link>
         </div>
     )
 }
