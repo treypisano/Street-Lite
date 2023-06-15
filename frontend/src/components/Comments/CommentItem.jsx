4dce5d4b3e2aa67e1ba31a46b63adcba326554d5
Arvid Hossain <arvidh97@gmail.com>
2023-06-15 15:49:51 -0400

    idk anymore

--- frontend/src/components/Comments/CommentItem.jsx
+++ frontend/src/components/Comments/CommentItem.jsx
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import jwtFetch from "../../store/jwt";
 
 const CommentItem = ({ comment }) => {
+  const [author, setAuthor] = useState(null);
+
+  useEffect(() => {
+    const fetchUserById = async (userId) => {
+      try {
+        const res = await jwtFetch(`/api/users/${userId}`);
+        if (res.ok) {
+          const author = await res.json();
+          setAuthor(author);
+        }
+      } catch (error) {
+        console.error("Failed to fetch user:", error);
+      }
+    };
+    fetchUserById(comment.userId);
+  }, []);
+
   return (
     <div>
       <p>{comment.body}</p>
-      <p>Posted by: {comment.userId}</p>
+      <p>Posted by: {author ? author.username : "Unknown"}</p>
     </div>
   );
 };
