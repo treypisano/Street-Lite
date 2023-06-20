290438b6f3746a4e4058ef2b78ebf666528f45ee
Mohammad Naqvi <mohammadnaqvi25@gmail.com>
2023-06-20 10:43:13 -0400

    personal links

--- frontend/src/components/BottomFooter/BottomFooter.js
+++ frontend/src/components/BottomFooter/BottomFooter.js
@@ -24,9 +24,9 @@ const BottomFooter = () => {
         </ul>
         <ul className="bottom-footer-col">
           <h3 className='bottom-footer-title'>Mohammad Naqvi</h3>
-          <li><a href="https://github.com/">GitHub</a></li>
-          <li><a href="https://www.linkedin.com/">LinkedIn</a></li>
-          <li><a href="https://wellfound.com/">Wellfound</a></li>
+          <li><a href="https://github.com/mxnaqvi">GitHub</a></li>
+          <li><a href="https://www.linkedin.com/in/mohammadalinaqvi/">LinkedIn</a></li>
+          <li><a href="https://wellfound.com/u/mohammad-naqvi-6">Wellfound</a></li>
         </ul>
         <ul className="bottom-footer-col">
           <h3 className='bottom-footer-title'>Technologies Used</h3>
